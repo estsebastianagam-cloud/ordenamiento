7c03f2483f3db0b3f9633e0180cf37e09a0b069f
estsebastianagam-cloud <est.sebastian.agam@unimilitar.edu.co>
2026-04-07 10:35:03 -0500

    Se elimino el metodo de ordenamiento manual ya que ya no se utiliza

--- ordenamiento/src/main/java/com/proyecto/ordenamiento/Ordenamiento.java
+++ ordenamiento/src/main/java/com/proyecto/ordenamiento/Ordenamiento.java
@@ -26,16 +26,6 @@ public class Ordenamiento {  //Se usara Arrays.sort() en lugar del metodo manual
         Arrays.sort(arr);
         System.out.println("Arreglo ordenado: " + Arrays.toString(arr));
     }
-
-    private static void ordenar(int v[]) {
-        for (int i = 0; i < v.length - 1; i++) {
-            for (int j = i + 1; j < v.length; j++) {
-                if (v[i] > v[j]) {
-                    int w = v[j];
-                    v[j] = v[i];
-                    v[i] = w;
-                }
-            }
-        }
-    }
 }
+
+    
