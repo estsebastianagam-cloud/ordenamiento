b7d18cb71f059f8d88cdd01f5e477e5851d76f44
estsebastianagam-cloud <est.sebastian.agam@unimilitar.edu.co>
2026-04-07 09:57:22 -0500

    Se agrego la opcion para que el usuario elija cuantos datos quiere ingresar

--- ordenamiento/src/main/java/com/proyecto/ordenamiento/Ordenamiento.java
+++ ordenamiento/src/main/java/com/proyecto/ordenamiento/Ordenamiento.java
@@ -1,16 +1,26 @@
 
-
 package com.proyecto.ordenamiento;
 
+import java.util.Scanner;
+
 public class Ordenamiento {
+    
     public static void main(String[] args) {
-        int arr[] = {5, 2, 11, -8, 115, 56, -3};
+        
+         Scanner sc = new Scanner(System.in);
+
+        System.out.print("¿Cuantos datos desea ingresar? ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
         ordenar(arr);
         System.out.println(java.util.Arrays.toString(arr));
     }
+
     private static void ordenar(int v[]) {
-        for (int i = 0; i < v.length-1; i++) {
-            for (int j = i+1; j < v.length; j++) {
+        for (int i = 0; i < v.length - 1; i++) {
+            for (int j = i + 1; j < v.length; j++) {
                 if (v[i] > v[j]) {
                     int w = v[j];
                     v[j] = v[i];
